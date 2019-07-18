334410ffbaee85985795d51def82d0007255b2e3
TheProrok29 <tomaszdbogacki@gmail.com>
2019-07-18 15:04:18 +0200

    Refactor and reorganize file
    * Run rainbow animation on separate thread

--- gui_led.py
+++ gui_led.py
@@ -5,6 +5,7 @@ import argparse
 
 from Tkinter import *
 from tkColorChooser import askcolor
+import threading 
 
 # LED strip configuration:
 LED_COUNT      = 28      # Number of LED pixels.
@@ -16,12 +17,16 @@ LED_BRIGHTNESS = 128     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
-r = 0;
-g = 0;
-b = 0;
+r = 0; # Red colour
+g = 0; # Green colour
+b = 0; # Blue colour
+
+flag_animation_run = False # Global flag- animation run
+flag_animation_stop = False # Global flag  animation stop
 
 # Create NeoPixel object with appropriate configuration.
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+
 # Intialize the library (must be called once before other functions).
 strip.begin()
     
@@ -32,29 +37,41 @@ def colorWipe(strip, color, wait_ms=50):
         strip.setPixelColor(i, color)
         strip.show()
         time.sleep(wait_ms/1000.0)
-
+        
+# Get colour using tkinter color chooser
 def getColor():
     global r, g, b
     color = askcolor(color=(g, r, b)) 
-    print(color)
     grb = color[0]
     print(grb)
     if grb != None:
         g = grb[0]
-        print ('r: ',g)
         r = grb[1]
-        print ('g: ',r)
         b = grb[2]
-        print ('b: ',b)
-        print("set RGB LED")
         colorWipe(strip, Color(r, g, b))
 
+# Power off leds
 def offLeds():
     colorWipe(strip, Color(0,0,0))
-    
+ 
+# Run rainbow animation on separate thread
+def startRainbow():
+    thread = threading.Thread(target=onRainbow)
+    thread.start()
+
+# Set flags to run animation
 def onRainbow():
+    global flag_animation_stop
+    global flag_animation_run
+    flag_animation_stop = False
+    flag_animation_run = True
     rainbow(strip)
     rainbowCycle(strip)
+ 
+# Set flags to stop animation
+def stopAnimation():
+    global flag_animation_stop
+    flag_animation_stop = True
 
 def theaterChase(strip, color, wait_ms=50, iterations=10):
     """Movie theater light style chaser animation."""
@@ -86,13 +103,22 @@ def rainbow(strip, wait_ms=20, iterations=1):
         strip.show()
         time.sleep(wait_ms/1000.0)
 
-def rainbowCycle(strip, wait_ms=20, iterations=5):
+def rainbowCycle(strip, wait_ms=20, iterations=1):
     """Draw rainbow that uniformly distributes itself across all pixels."""
-    for j in range(256*iterations):
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
-        strip.show()
-        time.sleep(wait_ms/1000.0)
+        
+    global flag_animation_run
+    global flag_animation_stop
+    while(True):
+        if (flag_animation_run == True):
+            for j in range(256*iterations):
+                for i in range(strip.numPixels()):
+                    strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+                strip.show()
+                time.sleep(wait_ms/1000.0)
+        if (flag_animation_stop == True):
+            colorWipe(strip, Color(0,0,0))
+            return
+    
 
 def theaterChaseRainbow(strip, wait_ms=50):
     """Rainbow movie theater light style chaser animation."""
@@ -105,21 +131,23 @@ def theaterChaseRainbow(strip, wait_ms=50):
             for i in range(0, strip.numPixels(), 3):
                 strip.setPixelColor(i+q, 0)
 
-
                 
 if __name__ == '__main__':
     window = Tk()
     window.title("LED WS2812 options")
     window.geometry('400x300')
-    lbl = Label(window, text="Wybierz kolor podświetlenia")
-    lbl.grid(column=0, row=0)
-    btn = Button(window, text="Paleta kolorów", command=getColor)
-    btn.grid(column=1, row=0)
     
-    lbl = Label(window, text="Efekt tenczy")
-    lbl.grid(column=0, row=1)
-    btn = Button(window, text="Włącz", command=onRainbow)
-    btn.grid(column=1, row=1)
+    lblColour = Label(window, text="Wybierz kolor podświetlenia")
+    lblColour.grid(column=0, row=0)
+    btnColour = Button(window, text="Paleta kolorów", command=getColor)
+    btnColour.grid(column=1, row=0)
+    
+    lblRainbow = Label(window, text="Efekt tęczy")
+    lblRainbow.grid(column=0, row=1)
+    btnRainbowOn = Button(window, text="Włącz", command=startRainbow)
+    btnRainbowOn.grid(column=1, row=1)
+    btnRainbowOff = Button(window, text="Wyłącz", command=stopAnimation)
+    btnRainbowOff.grid(column=2, row=1)
     
     lbl = Label(window, text="Kliknij jeśli chcesz wyłączyć")
     lbl.grid(column=0, row=4)
@@ -127,26 +155,4 @@ if __name__ == '__main__':
     btn.grid(column=1, row=4)
     
     window.mainloop()
-    
  
-
-    try:
-
-        while True:
-#            print ('Color wipe animations.')
-#            colorWipe(strip, Color(255, 0, 0))  # Red wipe
-#            colorWipe(strip, Color(0, 255, 0))  # Blue wipe
-#            colorWipe(strip, Color(0, 0, 255))  # Green wipe
-#            print ('Theater chase animations.')
-#            theaterChase(strip, Color(127, 127, 127))  # White theater chase
-#            theaterChase(strip, Color(127,   0,   0))  # Red theater chase
-#            theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
-            print ('Rainbow animations.')
-            rainbow(strip)
-            print ('Rainbow cycle.')
-            rainbowCycle(strip)
-            print ('Theater case.')
-            theaterChaseRainbow(strip)
-
-    except KeyboardInterrupt:
-        colorWipe(strip, Color(0,0,0), 10)
